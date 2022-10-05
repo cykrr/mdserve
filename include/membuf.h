c985cb344e5b5468ebfbdf79731db2f7e482ca48
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-10-05 15:55:03 -0300

    docs: Comentar membuffer

--- include/membuf.h
+++ include/membuf.h
@@ -2,15 +2,19 @@
 
 struct membuffer {
   char* data;
-  size_t asize;
-  size_t size;
+  size_t asize; // total
+  size_t size; // utilizado
 };
 
+/* Crea un membuffer */
 void membuf_init(struct membuffer* buf, size_t new_asize);
 
+/* Inserta una cadena en un membuffer */
 void membuf_append(struct membuffer* buf, const char* data, size_t size);
 
+/* Aumenta el tamaño de un membuffer */
 void membuf_grow(struct membuffer* buf, size_t new_asize);
 
+/* Libera la memoria de un membuffer */
 void membuf_fini(struct membuffer* buf);
 
